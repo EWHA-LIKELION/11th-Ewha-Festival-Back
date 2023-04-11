cbdb5be67bedbec55ee90152d6cb8bb43955a876
chaeyoung <yimcha2y0ung@gmail.com>
2023-04-11 20:21:00 +0900

    fix: 한글 깨짐 해결

--- festival/settings/base.py
+++ festival/settings/base.py
@@ -130,7 +130,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'ko-KR'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
