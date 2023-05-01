b3900a20623cf9109bb7dba042f42f94074ff6fd
chaeyoung <yimcha2y0ung@gmail.com>
2023-05-01 17:20:28 +0900

    fix: 부스 디테일 수정

--- booth/urls.py
+++ booth/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('<int:pk>/likes/', LikeView.as_view()),
     path('<int:pk>/comments/', CommentView.as_view()),
     path('<int:pk>/comments/<int:comment_pk>/', CommentDetailView.as_view()),
-    path('<int:pk>/notices/<int:notice_pk>/', NoticeDetailView.as_view()),
 ]
