46eb9f2a8e42648d86ba8b468d776e0547a8419a
chaeyoung <yimcha2y0ung@gmail.com>
2023-05-02 20:10:01 +0900

    fix: 좋아요한 부스 조회 api 수정

--- account/views.py
+++ account/views.py
@@ -60,15 +60,10 @@ class LikedListView(views.APIView):
 
     def get(self, request):
         user = request.user
-        keyword= request.GET.get('keyword')
-        
-        booths = (Booth.objects.filter(like=user.id) & (Booth.objects.filter(category__icontains=keyword) | 
-                           Booth.objects.filter(day__day__icontains=keyword) | Booth.objects.filter(college__icontains=keyword))).distinct()
+        booths = Booth.objects.filter(like=user.id)
 
         for booth in booths:
             booth.is_liked=True
-
-        total = booths.__len__()
             
         serializer = self.serializer_class(booths, many=True)
 
