9464303d02cd545f55c9c9d3c931b2e1b96fb023
chaeyoung <yimcha2y0ung@gmail.com>
2023-05-04 11:17:04 +0900

    feat: cors 주소 추가

--- festival/settings/base.py
+++ festival/settings/base.py
@@ -74,8 +74,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3000',
-    'https://ewha2023.com',
-    'https://api.ewha2023.com',
+    'https://smilewha2023.com',
+    'https://api.smilewha2023.com',
     'https://2023-ewha-festival.vercel.app'
 ]
 
